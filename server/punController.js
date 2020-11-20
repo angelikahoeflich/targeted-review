f88954c21efc0681ebc6f85fb6921e57c9623864
Angelika Hoeflich <angelikahoeflich@Angelikas-MacBook-Air.local>
2020-11-20 11:02:58 -0800

    server working

--- server/punController.js
+++ server/punController.js
@@ -19,12 +19,40 @@ const db = req.app.get('db')
 const db = req.app.get('db')
     const {content} = req.body
     const {user_id} = req.session.user
-    const puns = await db.add_pun([content, user_id])
+
+    try{
+      const puns = await db.add_pun([content, user_id])
+
+    } catch(err){
+      console.log('error adding pun, bugs need squashing')
+    }
   },
   editPun: async (req, res) => {
 const db = req.app.get('db')
+    //id refers to pun id
+    const {id} = req.params
+    const {content} = req.body
+
+    try{
+      const puns = await db.edit_pun([+id, content])
+      res.status(200).send(puns)
+    }
+    catch(err){
+      console.log('you cannot improve upon perfection', err);
+      res.sendStatus(500)
+    }
+
   },
   deletePun: async (req, res) => {
-const db = req.app.get('db')
-  },
+  const db = req.app.get('db')
+  const {id} = req.params
+  try{
+    const puns = await db.delete_pun(+id)
+    res.status(200).send(puns)
+  }
+  catch(err){
+    console.log(`Gourdn't delete such a masterpiece`)
+    res.sendStatus(500)
+  }
+  }
 }
